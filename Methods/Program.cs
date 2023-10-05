2f3be6d0a33d3af8244b3f5a0a1f9f7d9d0da86a
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-05 05:46:25 +0400

    Task2 and Task3 added

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,21 +5,68 @@
         static void Main(string[] args)
         {
             //Task 1
-            int[] arr = { 5, 10, 55, 325, 1 };
-            Console.WriteLine(MinM(arr));
+            //int[] arr = { 5, 10, 55, 325, 1 };
+            //Console.WriteLine(Minm(arr));
 
+            //Task 2
+            //float rad = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Çevrənin sahəsi {Sahe(rad)}");
+
+            //float width = float.Parse(Console.ReadLine());
+            //float height = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Düzbucaqlının sahəsi {Sahe(width,height)}");
+
+            //float a = float.Parse(Console.ReadLine());
+            //float b = float.Parse(Console.ReadLine());
+            //float c = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Düzbucaqlı paralelpipedin tam səthinin sahəsi {Sahe(a,b,c)}");
+
+            float radius = float.Parse(Console.ReadLine());
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi {Sahe(radius,a,b,c)}");
         }
 
         // TASK 1
-        public static int MinM(int[] arr)
+        //public static int Minm(int[] arr)
+        //{
+        //    int min = arr[0];
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] < min)
+        //            min = arr[i];
+        //    }
+        //    return min;
+        //}
+
+
+        // TASK 2
+        public static float Sahe(float rad)
         {
-            int min = arr[0];
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] < min)
-                    min = arr[i];
-            }
-            return min;
+            int p = 3;
+            return p * (float)Math.Pow(rad, 2);
         }
+
+        public static float Sahe(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Sahe(float a, float b, float c)
+        {
+            return 2 * (a * b + b * c + a * c);
+        }
+
+        public static float Sahe(float rad, float a, float b, float c)
+        {
+            float s = ((a + b + c) / 2)*rad;
+
+            return s;
+        }
+
+
+        //Task 3 Dərsdə yazılıb
+
     }
 }
