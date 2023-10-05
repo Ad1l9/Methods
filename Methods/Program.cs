b3e0f273e9f224a424c50325acddb21de17d6a49
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-05 05:33:49 +0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            int[] arr = { 5, 10, 55, 325, 1 };
+            Console.WriteLine(MinM(arr));
+
+        }
+
+        // TASK 1
+        public static int MinM(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            return min;
+        }
+    }
+}
